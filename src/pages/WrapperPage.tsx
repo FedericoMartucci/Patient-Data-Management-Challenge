5ed543d0fe7a1c319d60771e6962a0340905ede8
FedericoMartucci <federicomartucci@hotmail.com>
2025-01-15 15:54:33 -0300

    Change favicon and add logos.

--- src/pages/WrapperPage.tsx
+++ src/pages/WrapperPage.tsx
@@ -1,8 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const WrapperPage = (): JSX.Element => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen bg-[url('/BackgroundImage.png')] bg-cover">
+    <div className="relative flex flex-col justify-center items-center h-screen w-screen bg-[url('/BackgroundImage.png')] bg-cover">
+      <div className="absolute top-[16px]">
+        <Link to={"/"}>
+          <img className="max-w-[250px] w-[100%]" src="/transparent-medify-logo-white.png" alt="Medify Logo" />
+        </Link>
+      </div>
       <div className="flex items-center justify-center w-full py-10 mx-10">
         <div className="flex flex-col items-center justify-between w-full max-w-[90%] bg-extrawhite rounded-3xl p-10 max-h-[650px] h-full">
           <Outlet />
