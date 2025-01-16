136d8ff28d48f3cda1acefdf90b5981266c92a47
FedericoMartucci <federicomartucci@hotmail.com>
2025-01-16 16:30:17 -0300

    Fix PatientDetailModal behavior when clicking outside.

--- src/components/Modal/PatientDetailModal/PatientDetailModal.tsx
+++ src/components/Modal/PatientDetailModal/PatientDetailModal.tsx
@@ -14,14 +14,14 @@ const PatientDetailModal: FC<PatientDetailModalProps> = ({
 }): JSX.Element => {
   return (
     <Modal onClose={onClose} show={show}>
-      <div className="flex flex-col justify-center w-[90%] h-full">
+      <div className="flex flex-col justify-center w-[90%] h-[70%]">
         <Button
           onClick={onClose}
           className="rotate-90 bg-transparent active:bg-transparent hover:bg-transparent absolute top-0 left-0 rounded-full my-6 mx-4"
         >
           <Icon name="ChevronIcon" width="32" height="32" />
         </Button>
-        <div className="flex flex-col h-[70%] my-10 items-center justify-center w-full bg-extrawhite rounded-xl">
+        <div className="flex flex-col h-full py-4 items-center justify-center w-full bg-extrawhite rounded-xl">
           <div className="w-full h-full p-8 flex flex-col gap-10 bg-extrawhite rounded-xl">
             <PatientDetail />
           </div>
